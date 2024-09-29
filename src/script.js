376c2c1a9e2989dc0fd3d9ae3ec9b7a1c7dd13a1
creeper82 <66107554+creeper82@users.noreply.github.com>
2024-09-29 21:09:00 +0200

    Update everything, add hardcore mode and css

--- src/script.js
+++ src/script.js
@@ -1,178 +1,136 @@
 function RandomInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 
-window.onload = function() {
-	// Get the HTML Canvas
-  const canvas = document.getElementById("image_canvas");
-  // Get the canvas context
-  const ctx = canvas.getContext('2d');
-  
-  const coordinates = document.getElementById('coordinates');
-  const color = document.getElementById('color');
+class Region {
+    constructor(name, rgb) {
+        this.name = name;
+        this.rgb = rgb;
+    }
+}
 
-  const name = document.getElementById('name');
-  const points = document.getElementById('points');
+window.onload = function () {
+    // Get the HTML Canvas
+    const canvas = document.getElementById("image_canvas");
+    // Get the canvas context
+    const ctx = canvas.getContext('2d');
 
-  const chosen = document.getElementById('chosen');
-  
-  // Set this variable to any image source
-  const IMAGE_SRC = 'makroregionyRaw.png';
+    const name = document.getElementById('name');
+    const points = document.getElementById('points');
 
-  const colorArray = ["89;78;237", 
-    "56;45;207", 
-    "16;5;162",
-    "255;57;249",
-    "174;63;171",
-    "186;86;183",
-    "216;81;212",
-    "255;89;250",
-    "246;136;243",
-    "213;118;210",
-    "120;3;116",
-    "180;52;176",
-    "207;18;201",
-    "255;185;249",
-    "240;18;233",
-    "93;37;81",
-    "138;96;129",
-    "129;71;18",
-    "162;87;18",
-    "189;91;3",
-    "255;36;208",
-    "228;17;0",
-    "255;122;112",
-    "246;79;65",
-    "165;75;68",
-    "189;76;81",
-    "246;185;84",
-    "255;0;11",
-    "225;115;115",
-    "192;60;60",
-    "186;161;128",
-    "210;146;63",
-    "138;106;65",
-    "213;106;8",
-    "189;112;42",
-    "255;176;104",
-    "219;144;76",
-    "255;217;81",
-    "219;183;52",
-    "192;155;18",
-    "228;204;118",
-    "57;53;0",
-    "72;70;35",
-    "132;132;132",
-    "69;69;69",
-    "165;165;165",
-    "198;198;198",
-    "0;216;65",
-    "123;162;102",
-    "66;135;29",
-    "56;159;0",
-    "141;237;89",
-    "107;156;81",
-    "62;108;37",
-    "77;99;65",
-    "165;189;152",
-    "92;165;52",
-    "55;138;141",
-    "10;181;186"];
-  const nameArray = ["Pobrzeże Szczecińskie", 
-    "Pobrzeże Koszalińskie",
-    "Pobrzeże Grańskie",
-    "Pojezierze Wschodnio-pomorskie",
-    "Pojezierze Zachodniopomorskie",
-    "Pojezierze Połódniowopomorskie",
-    "Dolina Dolnej Wisły",
-    "Pojezierze Iławskie",
-    "Pojezierze Chełmińsko-Dobrzyńskie",
-    "Pradolina Toruńsko-Eberswaldzka",
-    "Pojezierze Wielkopolskie",
-    "Pojezierze Lubuskie",
-    "Pradolina Warciańsko-Odrzańska",
-    "Pojezierze Leszczyńskie",
-    "Wzniesienie Zielonogórskie",
-    "Obniżenie Dolnołużyckie",
-    "Wzniesienia Łużyckie",
-    "Obniżenie Milicko-Głogowskie",
-    "Nizina Południowowielkopolska",
-    "Wał Trzebnicki",
-    "Nizina Śląsko-Łużycka",
-    "Pogórze Zachodniosudeckie",
-    "Sudety Zachodnie",
-    "Sudety Środkowe",
-    "Przedgórze Sudeckie",
-    "Sudety Wschodnie",
-    "Nizina Śląska",
-    "Wyżyna Woźnicko-Wieluńska",
-    "Wyżyna Śląska",
-    "Wyżyna Krakowsko-Częstochowska",
-    "Niecka Nidziańska",
-    "Wyżyna Przedborska",
-    "Wyżyna Kielecka",
-    "Wzniesienia Południowomazowieckie",
-    "Nizina Srodkowomazowiecka",
-    "Nizina Północkomazowiecka",
-    "Nizina Południowopodlaska",
-    "Nizina Staropruska",
-    "Pojezierze Mazurskie",
-    "Pojezierze Litewskie",
-    "Nizina Północnopodlaska",
-    "Polesie Zachodnie",
-    "Polesie Wołyńskie",
-    "Wyżyna Wołyńska",
-    "Kotlina Pobuża",
-    "Roztocze",
-    "Wyżyna Lubelska",
-    "Kotlina Sandomierska",
-    "Brama Krakowska",
-    "Kotlina Oświęcimska",
-    "Kotlina Ostrawska",
-    "Pogórze Zachodniobeskidzkie",
-    "Beskidy Zachodnie",
-    "Obniżenie Orawsko-Podhalańskie",
-    "Łańcuch Tatrzański",
-    "Beskidy Środkowe",
-    "Pogórze Środkowobeskidzkie",
-    "Płaskowyż Sańsko-Dniestrzański",
-    "Beskidy Lesiste"];
+    const chosen = document.getElementById('chosen');
 
-	// Create a new image and add it to your canvas
-  const img = new Image();
-  img.height = 1000;
-  img.width =1000;
+    // Set this variable to any image source
+    const IMAGE_SRC = 'makroregionyRaw.png';
+
+    const regions = [
+        new Region("Pobrzeże Szczecińskie", "89;78;237"),
+        new Region("Pobrzeże Koszalińskie", "56;45;207"),
+        new Region("Pobrzeże Gdańskie", "16;5;162"),
+        new Region("Pojezierze Wschodnio-pomorskie", "255;57;249"),
+        new Region("Pojezierze Zachodniopomorskie", "174;63;171"),
+        new Region("Pojezierze Południowopomorskie", "186;86;183"),
+        new Region("Dolina Dolnej Wisły", "216;81;212"),
+        new Region("Pojezierze Iławskie", "255;89;250"),
+        new Region("Pojezierze Chełmińsko-Dobrzyńskie", "246;136;243"),
+        new Region("Pradolina Toruńsko-Eberswaldzka", "213;118;210"),
+        new Region("Pojezierze Wielkopolskie", "120;3;116"),
+        new Region("Pojezierze Lubuskie", "180;52;176"),
+        new Region("Pradolina Warciańsko-Odrzańska", "207;18;201"),
+        new Region("Pojezierze Leszczyńskie", "255;185;249"),
+        new Region("Wzniesienie Zielonogórskie", "240;18;233"),
+        new Region("Obniżenie Dolnołużyckie", "93;37;81"),
+        new Region("Wzniesienia Łużyckie", "138;96;129"),
+        new Region("Obniżenie Milicko-Głogowskie", "129;71;18"),
+        new Region("Nizina Południowowielkopolska", "162;87;18"),
+        new Region("Wał Trzebnicki", "189;91;3"),
+        new Region("Nizina Śląsko-Łużycka", "255;36;208"),
+        new Region("Pogórze Zachodniosudeckie", "228;17;0"),
+        new Region("Sudety Zachodnie", "255;122;112"),
+        new Region("Sudety Środkowe", "246;79;65"),
+        new Region("Przedgórze Sudeckie", "165;75;68"),
+        new Region("Sudety Wschodnie", "189;76;81"),
+        new Region("Nizina Śląska", "246;185;84"),
+        new Region("Wyżyna Woźnicko-Wieluńska", "255;0;11"),
+        new Region("Wyżyna Śląska", "225;115;115"),
+        new Region("Wyżyna Krakowsko-Częstochowska", "192;60;60"),
+        new Region("Niecka Nidziańska", "186;161;128"),
+        new Region("Wyżyna Przedborska", "210;146;63"),
+        new Region("Wyżyna Kielecka", "138;106;65"),
+        new Region("Wzniesienia Południowomazowieckie", "213;106;8"),
+        new Region("Nizina Środkowomazowiecka", "189;112;42"),
+        new Region("Nizina Północnomazowiecka", "255;176;104"),
+        new Region("Nizina Południowopodlaska", "219;144;76"),
+        new Region("Nizina Staropruska", "255;217;81"),
+        new Region("Pojezierze Mazurskie", "219;183;52"),
+        new Region("Pojezierze Litewskie", "192;155;18"),
+        new Region("Nizina Północnopodlaska", "228;204;118"),
+        new Region("Polesie Zachodnie", "57;53;0"),
+        new Region("Polesie Wołyńskie", "72;70;35"),
+        new Region("Wyżyna Wołyńska", "132;132;132"),
+        new Region("Kotlina Pobuża", "69;69;69"),
+        new Region("Roztocze", "165;165;165"),
+        new Region("Wyżyna Lubelska", "198;198;198"),
+        new Region("Kotlina Sandomierska", "0;216;65"),
+        new Region("Brama Krakowska", "123;162;102"),
+        new Region("Kotlina Oświęcimska", "66;135;29"),
+        new Region("Kotlina Ostrawska", "56;159;0"),
+        new Region("Pogórze Zachodniobeskidzkie", "141;237;89"),
+        new Region("Beskidy Zachodnie", "107;156;81"),
+        new Region("Obniżenie Orawsko-Podhalańskie", "62;108;37"),
+        new Region("Łańcuch Tatrzański", "77;99;65"),
+        new Region("Beskidy Środkowe", "165;189;152"),
+        new Region("Pogórze Środkowobeskidzkie", "92;165;52"),
+        new Region("Płaskowyż Sańsko-Dniestrzański", "55;138;141"),
+        new Region("Beskidy Lesiste", "10;181;186")
+    ];
+
+    // Create a new image and add it to your canvas
+    const img = new Image();
+    img.height = 700;
+    img.width = 800;
     // Wait for the image to load before drawing to the canvas
-    img.addEventListener('load', function() {
+    img.addEventListener('load', function () {
         ctx.drawImage(img, 0, 0);
     }, false);
 
-  // This is important to avoid cross origin errors
-  img.crossOrigin = "";
-  img.src = IMAGE_SRC;
-  
-  var randomId = RandomInt(nameArray.length);
-  name.innerText = nameArray[randomId];
-  var pointCounter = 0;
-  points.innerText = pointCounter;
-  
-  // Add a function to the mousemove event to get pixel data
-  // NOTE: You should probably debounce this
-  canvas.onmousedown = function(e) {
-    let mouseX = e.offsetX;
-    let mouseY = e.offsetY;
-    let colorData = ctx.getImageData(mouseX, mouseY, 1, 1).data;
-    let rgb = `${colorData[0]};${colorData[1]};${colorData[2]}`;
-    chosen.innerText =  nameArray[colorArray.indexOf(rgb)];
-    
-    //coordinates.innerText = `X: ${mouseX}, Y: ${mouseY}`;
-    //color.innerText = rgb;
+    // This is important to avoid cross origin errors
+    img.crossOrigin = "";
+    img.src = IMAGE_SRC;
 
-    if(colorArray[randomId] == rgb){
-      randomId = RandomInt(nameArray.length);
-      name.innerText = nameArray[randomId];
-      pointCounter+=1;
-      points.innerText = pointCounter;
+    let pointCounter = 0;
+
+    let randomRegion = regions[RandomInt(regions.length)];
+
+    name.innerText = randomRegion.name;
+    points.innerText = pointCounter;
+
+    canvas.onmousedown = function (e) {
+        let mouseX = e.offsetX;
+        let mouseY = e.offsetY;
+        let colorData = ctx.getImageData(mouseX, mouseY, 1, 1).data;
+        let rgb = `${colorData[0]};${colorData[1]};${colorData[2]}`;
+
+        const chosenRegion = regions.find(r => r.rgb === rgb);
+        if (chosenRegion === undefined) return;
+
+        chosen.innerText = "> " + chosenRegion.name;
+
+        if (randomRegion.rgb === rgb) {
+            randomRegion = regions[RandomInt(regions.length)];
+            name.innerText = randomRegion.name;
+            pointCounter += 1;
+            points.innerText = pointCounter;
+        }
+    };
+};
+
+const canvasContainer = document.querySelector("#canvas_container");
+
+document.querySelector("#hardcore_checkbox").addEventListener("change", e => {
+    if (e.currentTarget.checked) {
+        canvasContainer.classList.add("hardcore");
+    } else {
+        canvasContainer.classList.remove("hardcore");
     }
-  }  
-}
+});
