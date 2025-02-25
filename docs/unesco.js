1f1468285ed824b0d9c1ea896bee6db43b72d519
Bejmach <bejmach@gmail.com>
2025-02-25 18:28:46 +0100

    unesco update

---
+++ docs/unesco.js
@@ -0,0 +1,21 @@
+const regions = [
+	new Region("Park Mużakowski", "26;0;255"),
+	new Region("Kościoły pokoju", "39;15;255"),
+	new Region("Hala stulecia", "31;8;238"),
+	new Region("Aushwitz-Bierkenau", "48;28;223"),
+	new Region("Kalwaria zebrzydowska", "26;7;202"),
+	new Region("Kraków stare miasto", "97;79;255"),
+	new Region("Wieliczka", "72;56;213"),
+	new Region("Bochnia", "14;7;83"),
+	new Region("Drewniane kościoły małopolski", "37;21;174"),
+	new Region("Drewniane kościoły podkarpacia", "32;25;100"),
+	new Region("Drewniane cerkwie", "129;115;255"),
+	new Region("Zamość stare miasto", "80;68;195"),
+	new Region("Warszawa stare miasto", "12;0;119"),
+	new Region("Puszcza białowieska", "186;178;255"),
+	new Region("Toruń stare miasto", "122;115;187"),
+	new Region("Malbork zamek", "65;58;130 after.js:76:17"),
+    ];
+
+const IMAGE_SRC = 'unescoRaw.png';
+
